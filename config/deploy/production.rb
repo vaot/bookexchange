e7cdf07da6289620a6ae251e6a56f8d440e1aa41
davidmoradi <david@wishpond.com>
2015-10-04 01:08:29 -0700

    fix deployment issues

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,8 +3,8 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
-# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
+server 'ec2-52-10-106-240.us-west-2.compute.amazonaws.com', user: 'deploy', roles: %w{app db web}, primary: true
+server 'ec2-52-10-106-240.us-west-2.compute.amazonaws.com', user: 'deploy', roles: %w{app web}
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
 
