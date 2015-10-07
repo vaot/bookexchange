bfec4c05153369a939511504fa9dd01e5139e592
vaot <victorandrey_123@hotmail.com>
2015-10-06 20:27:30 -0700

    Forgot this guy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
 
       data.merge!(
         profile_picture: current_auth_user.profile_picture.attachment.url(:large),
-        auth_key: current_user.generate_or_last_active_auth_key.auth_key
+        auth_key: current_auth_user.generate_or_last_active_auth_key.auth_key
       )
 
       data[:logged_in] = true
