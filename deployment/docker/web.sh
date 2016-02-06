1c824a3292d8bcd6fb30341d3d4a76efb034a8ec
vaot <victorandrey_123@hotmail.com>
2016-02-06 13:47:55 -0800

    detach.

--- deployment/docker/web.sh
+++ deployment/docker/web.sh
@@ -1,6 +1,6 @@
 docker stop bookexchange
 docker rm bookexchange
-docker run --name bookexchange \
+docker run --name bookexchange -d \
        -e RAILS_ENV=production \
        -p 5005:80 \
        victorvaot/bookexchange /usr/bin/start-server.sh
